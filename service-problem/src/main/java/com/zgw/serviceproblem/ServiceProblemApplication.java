cd7095728a13ce1f20f0c57fdb12bf522744cc17
NepenthesZGW <2960571342@qq.com>
2020-06-15 12:39:38 +0800

    新建一个问题服务的项目

---
+++ service-problem/src/main/java/com/zgw/serviceproblem/ServiceProblemApplication.java
@@ -0,0 +1,16 @@
+package com.zgw.serviceproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ServiceProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProblemApplication.class, args);
+    }
+
+
+}
---
+++ service-problem/src/test/java/com/zgw/serviceproblem/ServiceProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgw.serviceproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
