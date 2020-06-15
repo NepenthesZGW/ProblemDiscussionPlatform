18a17213739b2c987dbf0ffcd8dea4ddd3c4156e
NepenthesZGW <2960571342@qq.com>
2020-06-15 13:30:30 +0800

    初始化项目，并作基本配置

---
+++ api-member/src/test/java/com/zgw/apimember/ApiMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgw.apimember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
