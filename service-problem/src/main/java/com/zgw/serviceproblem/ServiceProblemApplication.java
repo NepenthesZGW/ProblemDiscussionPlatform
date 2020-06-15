ac06f1b6f3dd31550c9a6e8c359d191b04ec3713
NepenthesZGW <2960571342@qq.com>
2020-06-15 13:06:47 +0800

    基本配置，可以把服务注册到eureka注册中心

--- service-problem/src/main/java/com/zgw/serviceproblem/ServiceProblemApplication.java
+++ service-problem/src/main/java/com/zgw/serviceproblem/ServiceProblemApplication.java
@@ -2,15 +2,19 @@ package com.zgw.serviceproblem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceProblemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceProblemApplication.class, args);
     }
+
 
 
 }
